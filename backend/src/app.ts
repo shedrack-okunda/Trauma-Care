885596fdd7aa0522504b3ee501c95625e80332b8
sheddy <okundashedrack@gmail.com>
2025-04-30 09:33:37 +0300

    Installed cors dependency

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,21 @@
 import express, { Request, Response } from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
 
 const app = express();
+
 const port = process.env.PORT || 5001;
+
+// middlewares
+const corsOptions = {
+  origin: process.env.ORIGIN,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
