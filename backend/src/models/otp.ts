014667faf9dc60ec02bb78c51adfa27bbeec7255
sheddy <okundashedrack@gmail.com>
2025-05-02 21:15:19 +0300

    Created a model schema for OTP

---
+++ backend/src/models/otp.ts
@@ -0,0 +1,20 @@
+import { Schema, model, Document } from "mongoose";
+
+export interface IOtp extends Document {
+  email: string;
+  otp: string;
+  createdAt: Date;
+  expiresAt: Date;
+  used: boolean;
+}
+
+const OtpSchema = new Schema<IOtp>(
+  {
+    email: { type: String, required: true },
+    otp: { type: String, required: true },
+    used: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+export const Otp = model<IOtp>("Otp", OtpSchema);
